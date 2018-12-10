9e0d6cf29355e88c3e837539f26abbe41a5ad97b
Isaac <hawkins.isaac@gmail.com>
2018-12-09 20:45:53 -0600

    add companies and user relationships

--- schema/schema.js
+++ schema/schema.js
@@ -1,17 +1,11 @@
 const graphql = require('graphql');
-const _ = require('lodash');
+const axios = require('axios');
 const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
   GraphQLSchema
 } = graphql;
-
-const users = [
-  { id: '23', firstName: "Bill", age: 53},
-  { id: '24', firstName: "Samantha", age: 23},
-  { id: '25', firstName: "George", age: 43},
-];
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -29,7 +23,7 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString }},
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id })
+        return axios.get(`http://localhost:3000/users/${args.id}`).then(res => res.data)
       }
     }
   }
